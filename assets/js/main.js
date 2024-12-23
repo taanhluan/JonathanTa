77d219453bebe4e52ca0cd767db4f8c755f17f10
Jonathan Ta <117909864+taanhluan@users.noreply.github.com>
2024-12-23 09:49:53 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
+function toggleContent(button) {
+  const content = button.parentElement.previousElementSibling;
+  content.classList.toggle('collapsed');
+  button.textContent = content.classList.contains('collapsed') ? 'Expand' : 'Collapse';
+}
 
